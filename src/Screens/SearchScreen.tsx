65953aa25a7720f117b01c4e4257c37748b234ed
koushal barick <koushal.barick@stockedge.com>
2025-01-28 20:52:01 +0530

    user can add the list from the modal input

--- src/Screens/SearchScreen.tsx
+++ src/Screens/SearchScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, FlatList, TouchableOpacity, } from 'react-native';
+import {View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
 import CompanyItem from '../Components/CompanyItem';
 import company from '../Utilities/company.json';
 import {Modal, TextInput} from 'react-native-paper';
@@ -7,12 +7,22 @@ import {AppProps} from '../Navigation/Navigation';
 
 const SearchScreen = ({navigation}: AppProps<'Search'>) => {
   const [text, setText] = React.useState('');
-  const [addList, setAddList] = React.useState('')
+  const [addList, setAddList] = React.useState('');
+  const [addCity, setAddCity] = React.useState('')
   const [visible, setVisible] = React.useState(false);
+  const [companies,setComapnies] = React.useState(company)
 
   const showModal = () => setVisible(true);
   const hideModal = () => setVisible(false);
-  const filteredCompanies = company.filter(item =>
+
+const handleAddCompanyName = () =>{
+  setComapnies(prev=>[{name:addList.trim(),city:addCity.trim()},...prev])
+  setAddList('')
+  setAddCity('')
+  hideModal()
+}
+
+  const filteredCompanies = companies.filter(item =>
     item.name.toLowerCase().includes(text.toLowerCase()),
   );
   return (
@@ -48,41 +58,43 @@ const SearchScreen = ({navigation}: AppProps<'Search'>) => {
         )}
       />
 
-      <TouchableOpacity onPress={showModal}
-        style={{
-          position: 'absolute',
-          bottom: 12,
-          right: 12,
-          backgroundColor: '#47535E',
-          borderRadius: 2,
-        }}>
+      <TouchableOpacity onPress={showModal} style={styles.modalButton}>
         <Text style={{color: 'white', padding: 8}}>Create List</Text>
       </TouchableOpacity>
 
-      <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={{backgroundColor: 'white', padding: 20, margin:12}}>
-          <Text>Add Your Favourite Company name</Text>
-          <TextInput
+      <Modal
+        visible={visible}
+        onDismiss={hideModal}
+        contentContainerStyle={{
+          backgroundColor: 'white',
+          padding: 20,
+          margin: 12,
+        }}>
+        <Text>Add Your Favourite Company name</Text>
+        <TextInput
           label="Enter Company Name"
           value={addList}
           mode="outlined"
           onChangeText={addList => setAddList(addList)}
-          style={{backgroundColor: 'white', marginTop:12}}
+          style={{backgroundColor: 'white', marginTop: 12}}
           placeholderTextColor="#0000"
           cursorColor="#000"
           outlineStyle={{borderColor: '#000'}}
         />
-        <TouchableOpacity onPress={showModal}
-        style={{
-         
-          marginTop:12,
-          alignItems:'center',
-          backgroundColor: '#47535E',
-          borderRadius: 2,
-          alignSelf:'flex-end'
-        }}>
-        <Text style={{color: 'white', padding: 8}}>Add List</Text>
-      </TouchableOpacity>
-        </Modal>
+        <TextInput
+          label="Enter City"
+          value={addCity}
+          mode="outlined"
+          onChangeText={addCity => setAddCity(addCity)}
+          style={{backgroundColor: 'white', marginTop: 12}}
+          placeholderTextColor="#0000"
+          cursorColor="#000"
+          outlineStyle={{borderColor: '#000'}}
+        />
+        <TouchableOpacity onPress={handleAddCompanyName} style={styles.addButton}>
+          <Text style={{color: 'white', padding: 8}}>Add List</Text>
+        </TouchableOpacity>
+      </Modal>
     </View>
   );
 };
@@ -93,6 +105,20 @@ const styles = StyleSheet.create({
     fontWeight: '400',
     margin: 16,
     textDecorationLine: 'underline',
+  },
+  modalButton: {
+    position: 'absolute',
+    bottom: 12,
+    right: 12,
+    backgroundColor: '#47535E',
+    borderRadius: 2,
+  },
+  addButton: {
+    marginTop: 12,
+    alignItems: 'center',
+    backgroundColor: '#47535E',
+    borderRadius: 2,
+    alignSelf: 'flex-end',
   },
 });
 
