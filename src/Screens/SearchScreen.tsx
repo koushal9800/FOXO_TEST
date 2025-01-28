fb93548bc7e9e5faee2eeec48ba9ebc25dcbabe6
koushal barick <koushal.barick@stockedge.com>
2025-01-28 16:33:44 +0530

    Button add

--- src/Screens/SearchScreen.tsx
+++ src/Screens/SearchScreen.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import {View, Text,StyleSheet, FlatList} from 'react-native';
+import {View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
 import CompanyItem from '../Components/CompanyItem';
 import company from '../Utilities/company.json';
 import {TextInput} from 'react-native-paper';
-import { AppNavigationProps, AppProps } from '../Navigation/Navigation';
+import {AppProps} from '../Navigation/Navigation';
 
-
-
-const SearchScreen = ({navigation}:AppProps<'Search'>) => {
+const SearchScreen = ({navigation}: AppProps<'Search'>) => {
   const [text, setText] = React.useState('');
   const filteredCompanies = company.filter(item =>
     item.name.toLowerCase().includes(text.toLowerCase()),
   );
   return (
-    <View>
+    <View style={{flex: 1}}>
       <View style={{width: '90%', margin: 16}}>
         <TextInput
           label="Search Comapany by Name"
@@ -28,21 +26,33 @@ const SearchScreen = ({navigation}:AppProps<'Search'>) => {
       </View>
       <Text style={styles.companyTitleStyle}>Popular Companies</Text>
       <FlatList
-  data={filteredCompanies}
-  keyExtractor={(item, index) => index.toString()}
-  renderItem={({ item }) => (
-    <CompanyItem
-      name={item.name}
-      cityName={item.city}
-      goToDetails={() =>
-        navigation.navigate('Details', {
-          title: item.name,
-          subTitle: item.city,
-        })
-      }
-    />
-  )}
-/>
+        data={filteredCompanies}
+        keyExtractor={(item, index) => index.toString()}
+        contentContainerStyle={{paddingBottom: 45}}
+        renderItem={({item}) => (
+          <CompanyItem
+            name={item.name}
+            cityName={item.city}
+            goToDetails={() =>
+              navigation.navigate('Details', {
+                title: item.name,
+                subTitle: item.city,
+              })
+            }
+          />
+        )}
+      />
+
+      <TouchableOpacity
+        style={{
+          position: 'absolute',
+          bottom: 12,
+          right: 12,
+          backgroundColor: '#47535E',
+          borderRadius: 2,
+        }}>
+        <Text style={{color: 'white', padding: 8}}>Create List</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -52,6 +62,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: '400',
     margin: 16,
+    textDecorationLine: 'underline',
   },
 });
 
